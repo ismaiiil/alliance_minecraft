732947ec4eed57a924c9a8ad9ac662effe066e22
ismaiiil <ismaiiil@live.com>
2021-04-01 00:37:47 +0400

    Initial commit

---
+++ src/main/java/com/ismaiiil/alliance/Alliance.java
@@ -0,0 +1,21 @@
+package com.ismaiiil.alliance;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Alliance extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+}
